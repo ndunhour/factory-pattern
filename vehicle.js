a129285ab0aab1b79e53c15bd20b7dee8f58af39
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-14 10:34:19 -1000

    create out outline function of getting request for mulitple vehicles

---
+++ vehicle.js
@@ -0,0 +1,14 @@
+function vehicleType(car, truck, minivan) {
+  vehicleType.make = "Toyota";
+}
+  //create the class of car
+  var vehicle = {
+    make: 'make',
+    model: 'model',
+    year: 1998
+  };
+
+  console.log(vehicle.make);
+
+  console.log(vehicleType(vehicle));
+
